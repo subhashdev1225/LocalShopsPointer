4f0d586bd7edee66763be7f86614692c2eaa235b
Subhash <subhash.kr.lal@gmail.com>
2019-11-29 15:14:36 +0530

    code commit

--- android/app/src/main/java/com/localshop/SplashActivity.java
+++ android/app/src/main/java/com/localshop/SplashActivity.java
@@ -2,6 +2,10 @@ package com.localshop;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.content.Intent;
+
+
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 5000);
     }
+
+
+
 }
